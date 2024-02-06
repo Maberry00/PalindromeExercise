d44e553b269d78042c407f5f2b594c1ff139263d
Maberry00 <maberry30@gmail.com>
2024-02-05 21:10:30 -0600

    wow

---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmithTests.cs
@@ -0,0 +1,32 @@
+﻿using PalindromeExercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PalindromeExerciseTests;
+
+public class WordSmithTests
+{
+    [Theory]
+    [InlineData("noon", true)]
+    [InlineData("wow", true)]
+    [InlineData("level", true)]
+    [InlineData("civic", true)]
+    [InlineData("poptart", false)]
+    [InlineData("bob", true)]
+    public void IsAPalindromeTest(string word, bool expected) 
+    {
+        //Arrange
+        var ws = new WordSmith();
+
+        //Act
+        var actual = ws.IsAPalindrom(word);
+
+        //Assert
+        Assert.Equal(expected, actual);
+
+    }
+}
